8af6913c2e679342d99f6f2c57f32314dd54be6c
killian vasse <killian.vasse@epitech.digital>
2021-05-06 11:11:23 +0200

    new calendar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="nav">
+      <img id="burger" src="./assets/burger.png" alt="menu burger">
+      <img id="logo" src="./assets/logo_take.png" alt="logo I take time">
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
+* {
+  margin: 0%;
+  padding: 0%;
+}
+html {
+  background-color: #2b323a;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  display: flex;
+  justify-content: space-around;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #aefffc;
+}
+
+#burger {
+  width: 2%;
+  padding: 0.6%;
+}
+
+#logo {
+  width:5%;
+  margin-left: 85%;
+  padding: 0.3%;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,448 @@
+<template>
+  <div class="home">
+    <div class="wrapper">
+      <div class="two">
+        <h1>AVR.</h1>
+        <p id="text">2021</p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="logue bi bi-chevron-left" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="logue bi bi-chevron-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+        <div class="gtrai"></div>
+        <div class="search">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="logue bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+          <div class="trai"></div>
+          <p>Recherche dans le calendrier</p>
+        </div>
+        <div class="gtrai"></div>
+        <div class="date">
+          <p>7</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" fill="currentColor" class="logue bi bi-calendar2-date" viewBox="0 0 16 16">
+            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/> 
+            <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z"/>
+          </svg>
+        </div>
+      </div>
+      <div class="three">
+        <div class="navcal">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="logue2 bi bi-chevron-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+          </svg>
+          <h1>AVR.</h1>
+          <p>2021</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="logue2 bi bi-chevron-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        </div>
+        <div class="day2">
+          <div class="jour">LUN.</div><div class="jour">MAR.</div><div class="jour">MER.</div><div class="jour">JEU.</div><div class="jour">VEN.</div><div class="jour">SAM.</div><div class="jour1">DIM.</div>
+        </div>
+        <div class="days2">
+          <div class="ung">29</div><div class="ung">30</div><div class="ung">31</div><div class="un">1</div><div class="un">2</div><div class="un">3</div><div class="und">4</div>      
+          <div class="un">5</div><div class="un">6</div><div class="una"><div class="aujour2"><p>7</p></div></div><div class="un">8</div><div class="un">9</div><div class="un">10</div><div class="und">11</div>
+          <div class="un">12</div><div class="un">13</div><div class="un">14</div><div class="un">15</div><div class="un">16</div><div class="un">17</div><div class="und">18</div>
+          <div class="uno2">19</div><div class="un">20</div><div class="un">21</div><div class="un">22</div><div class="un">23</div><div class="un">24</div><div class="und">25</div>
+          <div class="un">26</div><div class="un">27</div><div class="un">28</div><div class="un">29</div><div class="un">30</div><div class="ung">1</div><div class="ungd">2</div>
+        </div>
+        <div class="trai2"></div>
+        <div class="categories">
+          <div class="titlecat">
+            <h1>Mes catégories</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="logue2 bi bi-chevron-up" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+            </svg>
+          </div>
+          <div class="namecat">
+            <div class="chaque">
+              <div class="couleur1"></div> <p>Anniv</p>
+            </div>
+            <div class="chaque">
+              <div class="couleur2"></div> <p>Rappels</p>
+            </div>
+            <div class="chaque">
+              <div class="couleur3"></div> <p>Tasks</p>
+            </div>
+            <div class="chaque">
+              <div class="couleur4"></div> <p>RDV</p>
+            </div>
+            <div class="chaque">
+              <div class="couleur5"></div> <p>Objectifs</p>
+            </div>
+          </div>
+          <div class="titlecal">
+            <h1>Mes agendas</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="logue2 bi bi-chevron-up" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+            </svg>
+          </div>
+          <div class="namecat">
+            <div class="chaque">
+              <div class="couleur6"></div> <p>Pro</p>
+            </div>
+            <div class="chaque">
+              <div class="couleur7"></div> <p>Perso</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="four">
+        <Agenda />
+        <div class="day">
+          <div class="jour">LUNDI</div><div class="jour">MARDI</div><div class="jour">MERCREDI</div><div class="jour">JEUDI</div><div class="jour">VENDREDI</div><div class="jour">SAMEDI</div><div class="jour1">DIMANCHE</div>
+        </div>
+        <div class="days">
+          <div class="ung">29</div><div class="ung">30</div><div class="ung">31</div><div class="un">1</div><div class="un">2</div><div class="un">3</div><div class="und">4</div>      
+          <div class="un">5</div><div class="un">6</div><div class="una"><div class="aujour"><p>7</p></div></div><div class="un">8</div><div class="un">9</div><div class="un">10</div><div class="und">11</div>
+          <div class="un">12</div><div class="un">13</div><div class="un">14</div><div class="un">15 <div class="event"><div class="trai3"></div> <div>Anniv Posus</div></div> </div><div class="un">16</div><div class="un">17</div><div class="und">18</div>
+          <div class="uno">19</div><div class="un">20</div><div class="un">21</div><div class="un">22</div><div class="un">23</div><div class="un">24</div><div class="und">25</div>
+          <div class="un">26</div><div class="un">27</div><div class="un">28</div><div class="un">29</div><div class="un">30</div><div class="ung">1</div><div class="ungd">2</div>
+        </div>  
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Agenda from '../components/Agenda'
+
+export default {
+  name: 'Home',
+  components: {
+    Agenda
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+  transform: translate(-0.1%);
+  display: grid;
+  grid-template-columns: repeat(100, 1%);
+  grid-gap: 4px;
+  grid-auto-rows: minmax(5px, auto);
+}
+.two {
+  display: flex;
+  padding-left: 2%;
+  grid-column: 13 / 84;
+  grid-row: 1 / 8;
+  border-radius: 15px;
+  background-color: #353f48;
+  border: solid 1px #6e7378;
+}
+.trai {
+  border: solid 1px #aefffc;
+  height: 70%;
+  margin-bottom: auto;
+  margin-top: auto;
+  margin-left: 2%;
+}
+.trai2 {
+  border: solid 1px #aefffc;
+  width: 76%;
+  height: 0%;
+  margin-left: 15%;
+}
+.trai3 {
+  border: solid 2px #0fb14a;
+  width: 0%;
+  height: 13px;
+  margin-left: 10%;
+  margin-right: 5%;
+}
+.event {
+  display: flex;
+  margin-top: 5%;
+}
+.titlecat {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 4%;
+  margin-left: 4%;
+}
+.titlecat h1{
+  margin-bottom: auto;
+  margin-top: auto;
+  font-size: 80%;
+  color: #ffffff;
+}
+.titlecal {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 4%;
+  margin-left: 4%;
+}
+.titlecal h1{
+  margin-bottom: auto;
+  margin-top: 5%;
+  font-size: 80%;
+  color: #ffffff;
+}
+.namecat {
+  display: flex;
+  flex-direction: column;
+}
+.namecat p{
+  color: #ffffff;
+  font-size: 85%;
+}
+.couleur1 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #0fb14a;
+}
+.couleur2 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #0f68b1;
+}
+.couleur3 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #a1b10f;
+}
+.couleur4 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #960fb1;
+}
+.couleur5 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #3db10f;
+}
+.couleur6 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #0f99b1;
+}
+.couleur7 {
+  margin-left: 20%;
+  height: 19px;
+  width: 7%;
+  border-radius: 5px;
+  background-color: #eeda29;
+}
+.chaque {
+  display: flex;
+  margin-top: 7%;
+}
+.chaque p{
+  margin-left: 10%;
+}
+.gtrai {
+  border: solid 1px #aefffc;
+  height: 70%;
+  margin-bottom: auto;
+  margin-top: auto;
+  margin-left: 2%;
+  margin-right: 1.5%;
+}
+.date {
+  display: flex;
+}
+.date svg{
+  color: #aefffc;
+}
+.date p{
+  transform: translate(250%, 40%);
+  font-size: 120%;
+  color: #aefffc;
+}
+.search {
+  display: flex;
+  margin-bottom: auto;
+  margin-top: auto;
+  border: solid 1px #aefffc;
+  width: 70%;
+  height: 70%;
+  border-radius: 11px;
+}
+.search p{
+  margin-bottom: auto;
+  margin-top: auto;
+  margin-left: 2%;
+  text-align: left;
+  color: #aefffc;
+}
+#numj {
+  color: #aefffc;
+}
+#text{
+  margin-bottom: auto;
+  margin-top: 1.4%;
+  margin-left: 1%;
+  margin-right: 1%;
+  font-size: 90%;
+  color: #aefffc;
+}
+.two h1{
+  margin-bottom: auto;
+  margin-top: auto;
+  font-size: 140%;
+  color: #aefffc;
+}
+.logue {
+  margin-bottom: auto;
+  margin-top: auto;
+  margin-left: 2%;
+  color:#ffffff;
+}
+.logue2 {
+  margin-bottom: auto;
+  margin-top: auto;
+  margin-left: 2%;
+  height: 50%;
+  color:#aefffc;
+}
+.three {
+  transform: translate(-10%);
+  grid-column: 1 / 14;
+  grid-row: 1 / 99;
+  border-radius: 15px;
+  background-color: #353f48;
+  border: solid 1px #6e7378;
+}
+.navcal h1{
+  margin-bottom: auto;
+  margin-top: auto;
+  font-size: 80%;
+  color: #aefffc;
+}
+.navcal {
+  display: flex;
+  margin-left: 30%;
+  grid-row: 1 / 10;
+}
+.navcal p{
+  margin-left: 5%;
+  margin-bottom: auto;
+  margin-top: auto;
+  font-size: 55%;
+  color: #aefffc;
+}
+.four {
+  display: grid;
+  grid-column: 13 / 84;
+  grid-row: 8 / 99;
+  border-radius: 11px;
+  padding-top: 0.3%;
+  padding-left: 1%;
+  background-color: #353f48;
+  border: solid 1px #6e7378;
+}
+
+.day {
+  color: #aefffc;
+  text-align: left;
+  font-size: 80%;
+  margin-top: 0.5%;
+  display: grid;
+  grid-gap: 4.5px;
+  border-radius: 11px;
+  height: 100%;
+  grid-template-columns: repeat(7, 14%);
+  grid-auto-rows: minmax(1px, auto);
+}
+.days {
+  display: grid;
+  font-size: 90%;
+  text-align: left;
+  grid-gap: 4.5px;
+  border-radius: 11px;
+  grid-template-columns: repeat(7, 14%);
+  grid-auto-rows: minmax(145px, auto);
+}
+.day2 {
+  color: #aefffc;
+  transform: translate(13%);
+  text-align: left;
+  font-size: 70%;
+  margin-top: 0.5%;
+  display: grid;
+  grid-template-columns: repeat(7, 12.4%);
+  grid-auto-rows: minmax(1px, auto);
+}
+.days2 {
+  display: grid;
+  transform: translate(13%);
+  font-size: 70%;
+  text-align: left;
+  grid-gap: 4.5px;
+  grid-template-columns: repeat(7, 11%);
+  grid-auto-rows: minmax(80px, 1%);
+}
+
+.un {
+  padding-left: 5%;
+  border-radius: 15px;
+  color: #ffffff;
+}
+.ung {
+  padding-left: 5%;
+  border-radius: 15px;
+  color: #ffffff60;
+}
+.und {
+  padding-left: 5%;
+  border-radius: 15px;
+  color: #da6446;
+}
+.ungd {
+  padding-left: 5%;
+  border-radius: 15px;
+  color: #da644660;
+}
+.uno{
+  padding-left: 5%;
+  border-radius: 15px;
+  color: #ffffff;
+  border: solid 1px #0fb14a;
+}
+.uno2{
+  padding-left: 5%;
+  border-radius: 5px;
+  color: #ffffff;
+  border: solid 1px #0fb14a;
+}
+.una {
+  padding-left: 5%;
+  border-radius: 15px;
+  color: #ffffff;
+}
+.aujour {
+  width: 12px;
+  padding-left: 2%;
+  border-radius: 4px;
+  background-color: #0fb14a;
+}
+.aujour2 {
+  width: 9px;
+  padding-left: 6%;
+  border-radius: 4px;
+  background-color: #0fb14a;
+}
+.jour {
+  border-radius: 15px;
+}
+.jour1 {
+  border-radius: 15px;
+  color: #da6446;
+}
+</style>
